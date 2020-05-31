81669fb4f3a35dca8095ae51b39ff0f7df2bb768
ShashwatVv <vaibhavshashwat@gmail.com>
2020-05-31 18:21:33 +0530

    New parameter 'Letters' has been added with a default value.

--- CaeserCipher.py
+++ CaeserCipher.py
@@ -1,20 +1,23 @@
 
 #CaeserCipher accepts 3 parameters
 #message,mode and key
-#mode: can be either 1(Encryption) or 2(Decryption)
-#key: any non-negative integer smaller than 26
+#mode: can be either 1(Encryption) or 2(Decryption).
+#key: any non-negative integer smaller than 26.
+#Letters: string of characters on which the cipher is based.
+#A default string of 26 English alphabets has already been provided.
 
 import pandas as pd
 Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def CaeserCipher(message, mode, key ):
+def CaeserCipher(message, mode, key, Letters = Letters ):
     message = message.upper()
     result = ''
+    n = len(Letters)
     ##if encryption
     if mode == 1:
         for x in message:
             if x in Letters:
-                num = (Letters.find(x) + key) %26
+                num = (Letters.find(x) + key) %n
                 x = Letters[num]
             else:
                 x = x
@@ -24,7 +27,7 @@ def CaeserCipher(message, mode, key ):
     elif mode == 2:
         for x in message:
             if x in Letters:
-                num =(Letters.find(x) - key)%26
+                num =(Letters.find(x) - key)%n
                 x = Letters[num]
             else:
                 x = x
@@ -34,7 +37,6 @@ def CaeserCipher(message, mode, key ):
     df.to_clipboard(index=False,header=False)
     print("Result has been copied to clipboard!!!")
     return result
-print(CaeserCipher("What is GitHub",1,3))
 
 
 
